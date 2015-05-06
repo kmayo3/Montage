8aed27bde73f69acd985a9b2ff7cef0fc1da5a17
kmayo3 <katlyn.mayo@ymail.com>
2015-05-05 23:00:09 -0600

    Hurt player script

---
+++ Assets/_scripts/HurtPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HurtPlayer : MonoBehaviour 
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Player.currentHealth -= 1;
+        }
+    }
+}
