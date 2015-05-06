17000c47e9d8a5cb38af441c20513d36ac29a8c1
kmayo3 <katlyn.mayo@ymail.com>
2015-05-05 23:45:47 -0600

    Menu

---
+++ Assets/_scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour 
+{
+    public Texture backgroundTexture;
+
+    public float textPlacementY1;
+    public float textPlacementY2;
+
+    public float textPlacementX1;
+    public float textPlacementX2;
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+
+        if(GUI.Button(new Rect(Screen.width*textPlacementX1, Screen.height * textPlacementY1,
+            Screen.width * .5f, Screen.height * .1f), "PLAY GAME"))
+        {
+            print("Clicked Play Game");
+        }
+        if (GUI.Button(new Rect(Screen.width * textPlacementX2, Screen.height * textPlacementY2,
+            Screen.width * .5f, Screen.height * .1f), "PLAY GAME"))
+        {
+            print("Clicked Options");
+        }
+    }
+}
